1d1525f1251b5031969f8ee64fe4dd4770636c46
YNoah <48812111+YNoah@users.noreply.github.com>
2021-11-09 12:38:03 -0600

    Add files via upload

---
+++ SQLQuery PropuestaConsultasFragmentacion.sql
@@ -0,0 +1,9 @@
+-- PROPUESTA DE CONSULTAS PARA MODELO DE FRAGMENTACION DEL SEGMENTO VENTAS
+
+-- Listar el numero de ventas por territorio
+-- Listar el numero de ventas por tienda 
+-- Listar el total de clientes que pertenecen a cada territorio
+-- Actualizar la oferta de llantas de montaña con un descuento del 40%
+-- Listar las ordenes realizadas debidas a anuncio de revista
+-- Listar el total de ordenes hechas por cada representante de ventas
+-- Agregar el producto "HL Road Frame - Black, 58" a la oferta "Descuento por volumen 11 a 14"
