19c9150212dae073fa3848814ee2fea2dc5ff03a
Daniel Gonzalez <enda0507@gmail.com>
2021-11-10 07:52:51 -0600

    Se añadieron 4 predicados junto con sus scripts para ejecutarlos.

--- SQLQuery PropuestaConsultasFragmentacion.sql
+++ SQLQuery PropuestaConsultasFragmentacion.sql
@@ -9,3 +9,24 @@
 -- Agregar el producto "HL Road Frame - Black, 58" a la oferta "Descuento por volumen 11 a 14"
 --La suma de total de venta por PersonID
 --Tienda con mas ventas realizadas por tarjeta de credito
+-- Actualizar nombre de tarjeta de credito de credito SuperiorCard a SCard
+update Sales.CreditCard
+set CardType = 'SCard'
+where CardType = 'SuperiorCard';
+
+-- Listado de registros donde el ID de territorio de clientes es 1 y 3
+select *
+from Sales.Customer
+where TerritoryID in (1,3)
+order by TerritoryID asc;
+
+-- Listado de ventas con costo mayor a 2000 y menor a 4000
+select *
+from Sales.SalesOrderHeader
+where TotalDue > 2000 and TotalDue < 4000
+order by TotalDue asc;
+
+-- Listado de cambio de moneda de USD a MXN
+select *
+from Sales.CurrencyRate
+where FromCurrencyCode = 'USD' and ToCurrencyCode = 'MXN';
