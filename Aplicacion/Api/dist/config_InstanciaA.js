b71e8dd427bf8050f6dcbb2e1d32bf0165093f39
sayuri-panama <spanamas1400@alumno.ipn.mx>
2021-12-11 12:02:49 -0600

    front

---
+++ Aplicacion/Api/dist/config_InstanciaA.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    SQL_DATABASE: 'AdventureWorks2017',
+    SQL_USER: 'sa',
+    SQL_PASSWORD: 'InstanciaA',
+    SQL_HOST: 'localhost\\InstanciaA'
+};
---
+++ Aplicacion/Api/dist/config_InstanciaB.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    SQL_DATABASE: 'prueba',
+    SQL_USER: 'sa',
+    SQL_PASSWORD: 'InstanciaB',
+    SQL_HOST: 'localhost\\InstanciaB'
+};
