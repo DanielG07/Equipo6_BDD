4b2dc5c9caf49d1b6912ddc099fc72a1619a269c
sayuri-panama <67478396+sayuri-panama@users.noreply.github.com>
2021-12-11 12:24:58 -0600

    Delete Aplicacion directory

--- Aplicacion/Api/dist/config_InstanciaA.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SQL_DATABASE: 'AdventureWorks2017',
-    SQL_USER: 'sa',
-    SQL_PASSWORD: 'InstanciaA',
-    SQL_HOST: 'localhost\\InstanciaA'
-};
